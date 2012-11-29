1761c8e568b4ff0b11fb893a45f1e5af696a5331
James Jia <jamesjia@berkeley.edu>
2012-11-28 21:24:18 -0800

    Initial Project. (Lat/long coordinates textfields only)

--- GeoNotification App/GeoNotification App/ViewController.h
+++ GeoNotification App/GeoNotification App/ViewController.h
@@ -5,9 +5,66 @@
 //  Created by XLab Developer on 11/21/12.
 //  Copyright (c) 2012 XLab Developer. All rights reserved.
 //
-
+/**
+ This application requires CoreLocation and MapKit libraries in order to build and run successfully; check link binary with libraries tab includes both libraries. The application allows the user to input coordinates (latitude,longitude) and a title in order to monitor that specified location in a radius of 100 meters.
+ */
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+/**
+ The UIViewController interface implements CLLocationManagerDelegate protocols in order to define the methods used to monitor user-defined regions. The user interface itself is simple: three fields to describe the region and a button to append the region to an array.
+ */
+@interface ViewController : UIViewController<CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
-@interface ViewController : UIViewController
+/**
+ This method implements additional functionality to its super's viewDidLoad method by instantiating the NSMutableArray that stores regions and performs additional initalizations. Finally, it starts the generation of updates that report the user's current location by calling startUpdatingLocation of the locationManager.
+ */
+- (void)viewDidLoad;
+
+/**
+ This method checks to see if the device supports regionMonitoring and if locationServices are enabled. If all checks pass, startMonitoringForRegion is called. Otherwise, an error message is displayed and the application ends.
+ */
+- (void) initializeRegionMonitoring:(NSArray*)geofences;
+
+/**
+ This method checks to ensure location services are enabled. If so, a locationManager is instantiated and its delegate is set to the viewController. Otherwise, a notfication is generated stating that the user needs to enable location services.
+ */
+- (void)initializeLocationManager;
+
+/**
+ This method sets the mapView's default location to be the Berkeley Area and monitors the user's location.
+ */
+- (void)initializeMap;
+
+/**
+ Once the user enters a region that is currently being monitored, an alert pops up via showRegionAlert.
+ */
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region;
+
+/**
+ This is a method solely for testing purposes. It creates a NSLog to confirm that the locationManager is now monitoring a new region.
+ */
+- (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region;
+
+/**
+ This method is called once the submit button is pressed.  It checks if all the UITextFields have characters in them. If not, it will show a UIAlertView to notify the user to complete all fields. Otherwise,  it converts all the characters in the textfield to its corresponding Double value (nonnumerical values default to 0). A CLRegion is instantiated with the specified latitude, longitude, title, and radius of 100 meters, and is appended to the _regionArray.  A UIAlertview is generated to show the user that the submission was successful, and the textfields are cleared.
+ */
+- (IBAction)appendRegionToArray:(UIButton *)sender;
+
+/**
+ This method creates a UIAlertView that displays an alert whenever the method is called.
+ */
+- (void) showRegionAlert:(NSString *)alertText forRegion:(NSString *)regionIdentifier;
+
+/**
+ This method shows an alert with a message passed in as a parameter (alertText).
+ */
+- (void)showAlertWithMessage:(NSString*)alertText;
+
+/**
+ This method hides the keyboard whenever the return key is pressed in a textfield.
+ */
+-(IBAction)textFieldReturn:(UITextField *)sender;
 
 @end
--- GeoNotification App/GeoNotification App/ViewController.m
+++ GeoNotification App/GeoNotification App/ViewController.m
@@ -7,23 +7,135 @@
 //
 
 #import "ViewController.h"
-
+/**
+ The view controller declares the existence of three UITextFields: latitude,longitude, and title. It declares a locationManager, whose delegate will be the View Controller itself, a NSMutableArray (_regionArray) to store the regions, and instantiates the mapView (the object that displays the map).
+ */
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *latitudeField;
+@property (weak, nonatomic) IBOutlet UITextField *longitudeField;
+@property (weak, nonatomic) IBOutlet UITextField *titleField;
 
 @end
 
 @implementation ViewController
+CLLocationManager *_locationManager;
+NSMutableArray *_regionArray;
+@synthesize mapView;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _regionArray=[[NSMutableArray alloc] init];
+    [self initializeMap];
+    [self initializeLocationManager];
+    [_locationManager startUpdatingLocation];
 }
 
-- (void)didReceiveMemoryWarning
+- (void) initializeRegionMonitoring:(NSArray*)geofences {
+    if(![CLLocationManager regionMonitoringAvailable]) {
+        [self showAlertWithMessage:@"This app requires region monitoring features which are unavailable on this device."];
+        return;
+    }
+    else if ([CLLocationManager authorizationStatus]!=kCLAuthorizationStatusAuthorized)
+    {
+        [self showAlertWithMessage:@"Please enable location services."];
+        return;
+    }
+    else{
+        for(CLRegion *geofence in geofences) {
+            [_locationManager startMonitoringForRegion:geofence];
+        }
+    }
+}
+
+- (void)initializeLocationManager {
+    if(![CLLocationManager locationServicesEnabled]) {
+        [self showAlertWithMessage:@"You need to enable location services to use this app."];
+        return;
+    }
+    
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+}
+
+- (void)initializeMap {
+    CLLocationCoordinate2D initialCoordinate;
+    initialCoordinate.latitude = 37.87;
+    initialCoordinate.longitude = 122.3;
+    
+    [self.mapView setRegion:MKCoordinateRegionMakeWithDistance(initialCoordinate, 400, 400) animated:YES];
+    self.mapView.centerCoordinate = initialCoordinate;
+    [self.mapView setUserTrackingMode:MKUserTrackingModeFollow animated:YES];
+}
+
+
+//Region Checks
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
+    [self showRegionAlert:@"Entering Region" forRegion:region.identifier];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
+    [self showRegionAlert:@"Exiting Region" forRegion:region.identifier];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region {
+    NSLog(@"Started monitoring %@ region", region.identifier);
+}
+
+
+//Helper Functions
+- (IBAction)appendRegionToArray:(UIButton *)sender {
+    if ([_latitudeField.text length]>0 && [_longitudeField.text length]>0 && [_titleField.text length]>0){
+        CLLocationDegrees latitude = _latitudeField.text.doubleValue;
+        CLLocationDegrees longitude = _longitudeField.text.doubleValue;
+        CLLocationCoordinate2D centerCoordinate = CLLocationCoordinate2DMake(latitude,longitude);
+        CLLocationDistance regionRadius = 100;
+        NSString *title = _titleField.text;
+        [_regionArray addObject:[[CLRegion alloc] initCircularRegionWithCenter:centerCoordinate
+                                                radius:regionRadius
+                                                                    identifier:title]];
+        UIAlertView *alertView = [[UIAlertView alloc]
+                                  initWithTitle:_titleField.text
+                                  message:@"Appended region" delegate:self
+                                  cancelButtonTitle:@"Confirm"
+                                  otherButtonTitles:nil];
+        [alertView show];
+        
+        [self initializeRegionMonitoring:_regionArray];
+        _titleField.text=nil;
+        _latitudeField.text=nil;
+        _longitudeField.text=nil;
+    }
+    else{
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Incomplete Fields"
+                                                            message:@"Complete all fields"
+                                                           delegate:self
+                                                  cancelButtonTitle:@"Ok"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+
+- (void) showRegionAlert:(NSString *)alertText forRegion:(NSString *)regionIdentifier {
+    UIAlertView *message = [[UIAlertView alloc] initWithTitle:alertText
+                                                      message:regionIdentifier
+                                                     delegate:nil
+                                            cancelButtonTitle:@"OK"
+                                            otherButtonTitles:nil];
+    [message show];
+}
+
+- (void)showAlertWithMessage:(NSString*)alertText {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Location Services Error"
+                                                        message:alertText
+                                                       delegate:self
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil];
+    [alertView show];
+}
+
+-(IBAction)textFieldReturn:(UITextField *)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [sender resignFirstResponder];
 }
-
 @end
